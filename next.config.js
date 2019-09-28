256cc75365abbdbb39be11c4ded4600104b7128b
Kathrine Gibson <kgzinnia@gmail.com>
2019-09-28 11:57:36 -0700

    Removed unnecessary typescript dependency

--- next.config.js
+++ next.config.js
@@ -1,21 +1,18 @@
-const withTypescript = require("@zeit/next-typescript");
 const withCSS = require("@zeit/next-css");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-module.exports = withTypescript(
-  withCSS({
-    webpack(config) {
-      if (process.env.ANALYZE) {
-        config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: "server",
-            analyzerPort: 8888,
-            openAnalyzer: true
-          })
-        );
-      }
-      return config;
-    },
-    cssModules: true
-  })
-);
+module.exports = withCSS({
+  webpack(config) {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "server",
+          analyzerPort: 8888,
+          openAnalyzer: true
+        })
+      );
+    }
+    return config;
+  },
+  cssModules: true
+});
