23d6455c1aafb2aa965785fe7f3ee2a560883998
maxchehab <maxchehab@gmail.com>
2019-09-28 21:01:22 +0000

    fix

--- api/tree/index.ts
+++ api/tree/index.ts
@@ -7,10 +7,10 @@ export default Lambda(async (req, res) => {
   const tree = TreeDTO.from(req);
   const { amount, charity, company } = tree;
 
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.USE_SLACK) {
     const slackClient = new WebClient(process.env.SLACK_TOKEN);
 
-    const slackResponse = await slackClient.chat.postMessage({
+    await slackClient.chat.postMessage({
       channel: "x-open-tree",
       text: `> Company *${company}* requested to plant *${amount}* trees with *${charity}*.`
     });
