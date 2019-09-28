f5ec043f998dc5fb02bd684f9f171fe4863a046a
Kathrine Gibson <kgzinnia@gmail.com>
2019-09-28 13:52:10 -0700

    Created slack bot

--- api/tree/index.ts
+++ api/tree/index.ts
@@ -1,8 +1,20 @@
+import { WebClient } from "@slack/web-api";
+
 import { TreeDTO } from "./dto/tree-request.interface";
 import { Lambda } from "../../common/util/lambda.util";
 
-export default Lambda((req, res) => {
+export default Lambda(async (req, res) => {
   const tree = TreeDTO.from(req);
+  const { amount, charity, company } = tree;
 
-  return res.status(200).json({ tree });
+  if (process.env.NODE_ENV === "production") {
+    const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
+    const slackResponse = await slackClient.chat.postMessage({
+      channel: "x-open-tree",
+      text: `> Company *${company}* requested to plant *${amount}* trees with *${charity}*.`
+    });
+  }
+
+  return res.status(200).json({});
 });
